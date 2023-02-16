5b801dc72056434252a6a54e9b25a8f9cf709aac
Isaiah-Mwinga <motongori@marketforce360.com>
2023-02-16 20:09:41 +0300

    auth

--- main.py
+++ main.py
@@ -65,3 +65,15 @@ class AuthBearer(HTTPBearer):
             if not self.verify_jwt(credentials.credentials):
                 raise HTTPException(status_code=403, detail="Invalid token or expired token")
             return credentials.credentials
+        else:
+            raise HTTPException(status_code=403, detail="Invalid authorization code.")
+
+    def verify_jwt(self, jwtoken: str) -> bool:
+        is_token_valid: bool = False
+        try:
+            payload = decode_jwt(jwtoken)
+        except:
+            payload = None
+        if payload:
+            is_token_valid = True
+        return is_token_valid
